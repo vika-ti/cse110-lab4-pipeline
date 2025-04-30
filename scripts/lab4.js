a54561c8730504ed102ae85de402c8dc4e0729b0
Victoria Timofeev <victoria.ti@yahoo.com>
2025-04-30 07:48:49 +0000

    Fix bugs in sumValues and discountPrices

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -7,7 +7,7 @@
  */
 function sumValues(num1, num2, add) {
     if (add) {
-        const result = 0;
+        let result = 0;
 
         result = num1 + num2;
 
@@ -25,11 +25,15 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
+    if (prices.length === 0) {
+        return false;
+    }
+    
     const discounted = []
     const length = prices.length;
-    let discountedPrice = 0
+    //let discountedPrice = 0
     for(let i = 0; i < length; i++) {
-        discountedPrice += prices[i] * (1 - discount);
+        let discountedPrice += prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
 
